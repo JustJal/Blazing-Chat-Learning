99459df24f575859cd73a57f083f55716fc225a8
Jalinson Diaz <jalinson5@gmail.com>
2021-07-26 14:49:32 -0400

    added hardcoded contacts

---
+++ BlazingChat/BlazingChat/Shared/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingChat.Shared
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Contact()
+        {
+
+        }
+        public Contact(int id, string firstName, string lastName)
+        {
+            (ID, FirstName, LastName) = (id, firstName, lastName);
+
+        }
+    }
+}
