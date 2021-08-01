54f804cc2695bee7316a40e6eabdea98f10d78a4
Jalinson Diaz <jalinson5@gmail.com>
2021-08-01 14:39:13 -0400

    now you can chat to one single person

---
+++ BlazingChat/BlazingChat/Client/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿export function setScroll() {
+    //let's fix scroll here
+    var divMessageContainerBase = document.getElementById('divMessageContainerBase');
+    if (divMessageContainerBase != null) {
+        divMessageContainerBase.scrollTop = divMessageContainerBase.scrollHeight;
+    }
+}
